bab3ad1c6b8372d3755520db7554941633c50f56
Justin Zhao <82178237+JzJoker@users.noreply.github.com>
2026-08-20 16:29:31 -0400

    Add: course-schedule - submission-10

---
+++ Data Structures & Algorithms/course-schedule/submission-10.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # jot down indegree and postreq of each class
+        indegree = [0] * numCourses
+        postreq = [[] for i in range(numCourses)]
+        finished = 0
+        for pair in prerequisites:
+            indegree[pair[0]] += 1
+            postreq[pair[1]].append(pair[0])
+
+        q = deque()
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                q.append(i)
+        while q:
+            course = q.popleft()
+            finished += 1
+            dependents = postreq[course]
+            for course in dependents:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    q.append(course)
+        return finished == numCourses
